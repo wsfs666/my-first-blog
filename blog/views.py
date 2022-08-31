8e8fb6d21b077bd1bce16931369328df20f7c94c
wsfs666 <wsfs666@gmail.com>
2022-08-31 11:56:18 -0700

    added view and template for detailed blog post as well as css for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+
 #.当前目录 models.py和views.py在同一目录下
 # Create your views here.
 
@@ -8,3 +9,7 @@ def post_list(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 #return render 方法渲染模板 blog/post_list.html 得到结果
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
